f11997606d05e3bee58038403510abbb243093ad
Anton Medvedev <anton.medvedev@mandosys.com>
2016-11-18 15:02:59 +0300

    #2: Webpack config (beta version)

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var path = require('path');
+
+var output = "./bin";
+
+module.exports = {
+    context: path.join(__dirname, "src/app"),
+    entry: {
+        'app': './app.ts'
+    },
+    output: {
+        path: output,
+        filename: "[hash].[name].js"
+    },
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    preLoaders: [
+        {
+            test: /\.(js|css)$/,
+            loader: 'source-map-loader',
+            exclude: []
+        },
+        {
+            test: /\.ts$/,
+            loader: 'tslint'
+        }
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'ts',
+                query: { configFileName: 'tsconfig.json' }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss!resolve-url!sass-loader?sourceMap')
+            }/*,
+            {
+                test: /\.(css|svg|png|jpg|jpeg|gif)$/,
+                loader: 'file?name=[path][name].[ext]'
+            }*/
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[hash].[name].css', { allChunks: true }),
+        new CleanWebpackPlugin([output]),
+        new CopyWebpackPlugin([
+            //{ from: 'images', to: "images" },
+            { from: "../short-news-list.json" },
+            { from: '**/*.html', to: "app" },
+            { from: "../../node_modules/core-js/client/shim.min.js", to: "libs" },
+            { from: "../../node_modules/zone.js/dist/zone.min.js", to: "libs" },
+            { from: "../../node_modules/reflect-metadata/Reflect.js", to: "libs" }
+        ]),
+        new HtmlWebpackPlugin({
+            template: '../index.html',
+            chunksSortMode: 'dependency'
+        })
+	]
+}
