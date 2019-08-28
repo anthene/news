26e713a4854edb10d21579b82dcc2dd31a3d8106
Anton Medvedev <anthene@mail.ru>
2019-08-27 21:26:25 -0700

    #36: 'data/short' api method to create a daily list of short news

--- Web/Controllers/DataController.cs
+++ Web/Controllers/DataController.cs
@@ -1,4 +1,12 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using static Newtonsoft.Json.JsonConvert;
+using static System.IO.File;
+using static System.IO.Path;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Web.Controllers
 {
@@ -6,6 +14,20 @@ namespace Web.Controllers
     [ApiController]
     public class DataController : Controller
     {
+        readonly string _webRootPath;
+        readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            }
+        };
+
+        public DataController(IHostingEnvironment env)
+        {
+            _webRootPath = env.WebRootPath;
+        }
+
         [HttpGet("news-list-{date}.json")]
         public IActionResult News(string date)
         {
@@ -17,5 +39,27 @@ namespace Web.Controllers
         {
             return Ok(new object[0]);
         }
+
+        [HttpPost("short")]
+        public IActionResult AddShortNews([FromBody] ShortNews[] shortNews)
+        {
+            var dates = shortNews.Select(sn => sn.Date.ToUniversalTime().Date).Distinct();
+            if (dates.Count() > 1)
+                return BadRequest("The short news list contains different dates.");
+
+            var date = dates.Single();
+            var fileName = Combine(_webRootPath, "data", $"short-news-list-{date.ToString("yyyyMMdd")}.json");
+
+            WriteAllText(fileName, SerializeObject(shortNews, _jsonSerializerSettings));
+
+            return Ok();
+        }
+    }
+
+    public class ShortNews
+    {
+        public DateTime Date { get; set; }
+
+        public string Header { get; set; }
     }
 }
